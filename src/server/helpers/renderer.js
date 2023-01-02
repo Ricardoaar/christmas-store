5d24f0660bf86aa5caf05d94a5e44c04ea790b34
RicardoRA <Ricardo.rito@ubidots.com>
2023-01-02 15:19:04 -0600

    :lipstick: Font: Add font montserrat

--- src/server/helpers/renderer.js
+++ src/server/helpers/renderer.js
@@ -58,6 +58,9 @@ const renderFullPage = (location, store, hashManifest) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Ricardoaar Store</title>
         <link rel="stylesheet" href="${mainStylesLocation}">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;800&display=swap" rel="stylesheet">
       <script src="https://kit.fontawesome.com/681847d319.js" crossorigin="anonymous"></script>
         
       </head>
